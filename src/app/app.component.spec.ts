5f5ed5a8b7247bc161d0b91fffe6dacb91189571
Yash Lokwani <lokwani.yash@concret.io>
2020-12-16 12:55:25 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chromeExt'`, () => {
+  it(`should have as title 'relocate-old-ext'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chromeExt');
+    expect(app.title).toEqual('relocate-old-ext');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('chromeExt app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('relocate-old-ext app is running!');
   });
 });
